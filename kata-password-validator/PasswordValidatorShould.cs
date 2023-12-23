e071edc9e7c1be5b9eb5fbf33ad1f5de7ef4e8be
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-23 16:49:52 +0000

    ❌ have_a_minimum_length

--- kata-password-validator/PasswordValidatorShould.cs
+++ kata-password-validator/PasswordValidatorShould.cs
@@ -9,5 +9,11 @@ namespace kata_password_validator
         {
             Assert.True(PasswordValidator.IsValid("holaMundo_1"));
         }
+        
+        [Fact]
+        public void have_a_minimum_length()
+        {
+            Assert.False(PasswordValidator.IsValid("aA_1"));
+        }
     }
 }
