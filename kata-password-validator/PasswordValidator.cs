246c860b2b25f596fa469d5371378116ddb9e169
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-23 16:46:23 +0000

    ❌ validate_all_rules

---
+++ kata-password-validator/PasswordValidator.cs
@@ -0,0 +1,11 @@
+namespace kata_password_validator
+{
+    public class PasswordValidator
+
+    {
+        public static bool IsValid(string password)
+        {
+            return false;
+        }
+    }
+}
---
+++ kata-password-validator/PasswordValidatorShould.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace kata_password_validator
+{
+    public class PasswordValidatorShould
+    {
+        [Fact]
+        public void validate_all_rules()
+        {
+            Assert.True(PasswordValidator.IsValid("holaMundo_1"));
+        }
+    }
+}
