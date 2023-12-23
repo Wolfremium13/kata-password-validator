be997629ec11a10005c9a6ea4aac9d6a0f924348
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-23 16:42:24 +0000

    🎉 initial commit

---
+++ kata-password-validator/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace kata_password_validator
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
